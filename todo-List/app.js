738545a7d2a1e7acaee2c9cc6dc4bd3ef8b4f81f
zaida <zcueto58@gmail>
2022-04-04 14:02:42 -0300

    To do lists funcionando perfeitamente

--- todo-List/app.js
+++ todo-List/app.js
@@ -1,26 +1,27 @@
-
-
-
 let bancoDeDadosLocalSoparaTeste = [
-    {'tarefa': 'estudar js', 'status' : ''},
-    {'tarefa': 'netflix js', 'status ': ''},
-    {'tarefa': 'css', 'status' : 'checked'},
-    {'tarefa': ' js', 'status' : 'checked'}
+    {'tarafa': 'estudar js', 'status': ''},
+    {'tarafa': 'css', 'status': ''},
+    {'tarafa': 'html', 'status': 'checked'}
 ]
+    
 
 
-const criarItem = (tarefa, status) => {
+// const getBanco = ( ) => JSON.parse(localStorage.getItem ('todoList')) ?? [];
+// const setBanco = ( bancoDeDadosLocalSoparaTeste) =>localStorage.setItem('todolist', JSON.stringify(bancoDeDadosLocalSoparaTeste));
+
+
+const criarItem = (tarefa, status, indice) => {
     const item  = document.createElement('label');
     item.classList.add('todo__item')
     item.innerHTML = `
-    <input type="checkbox" ${status}>
+    <input type="checkbox" ${status} data-indice=${indice}>
     <div>${tarefa}</div>
-    <input type="button" value="X">`
+    <input type="button" value="X"  data-indice=${indice}>`
     document.getElementById('todoList').appendChild(item)
 }
 
 const limparTarefas = ( ) =>{
-    const todoList = document.getElementById('todoList')
+    const todoList = document.getElementById('todoList');
     while(todoList.firstChild ){
         todoList.removeChild(todoList.lastChild);
     }
@@ -28,9 +29,61 @@ const limparTarefas = ( ) =>{
 }
 const  atulaizarTela = () => {
     limparTarefas()
-    bancoDeDadosLocalSoparaTeste.forEach(item => criarItem (item.tarefa, item.status))
+    // const bancoDeDadosLocalSoparaTeste = getBanco()
+    bancoDeDadosLocalSoparaTeste.forEach((item, indice) => criarItem (item.tarefas, item.status, indice));
+   
 }
-atulaizarTela();
-atulaizarTela();
-atulaizarTela();
 
+
+const adicionarTarefa = (evento)=> {
+    const tecla = evento.key;
+ 
+    if(tecla === 'Enter') {
+        //  const bancoDeDadosLocalSoparaTeste = getBanco()
+        const texto = evento.target.value
+        bancoDeDadosLocalSoparaTeste.push({'tarefas': texto, 'status' : ''});
+        // setBanco(bancoDeDadosLocalSoparaTeste)
+         atulaizarTela()
+    
+         evento.target.value = '';
+   
+    }
+ 
+}
+
+const removerItem = (indice)=>{
+    // const bancoDeDadosLocalSoparaTeste = getBanco()
+    bancoDeDadosLocalSoparaTeste.splice(indice,1)
+    // setBanco(bancoDeDadosLocalSoparaTeste)
+    atulaizarTela()
+}
+
+const atualizarItem = (indice) => {
+    // const bancoDeDadosLocalSoparaTeste = getBanco()
+    bancoDeDadosLocalSoparaTeste[indice].status = bancoDeDadosLocalSoparaTeste[indice].status === '' ? 'checked ': '';
+    // setBanco(bancoDeDadosLocalSoparaTeste)
+    atulaizarTela()
+}
+    
+
+
+
+const clickItem = (evento) =>{
+    const elemento = evento.target;
+   if(elemento.type === 'button'){
+       const indice = elemento.dataset.indice;
+       removerItem(indice)
+   }else if(elemento.type === 'checkbox'){
+    const indice = elemento.dataset.indice;
+       atualizarItem(indice);
+   }
+
+}
+
+
+document.getElementById('newItem').addEventListener('keypress', adicionarTarefa)
+ document.getElementById('todoList').addEventListener('click', clickItem)
+
+
+
+atulaizarTela();
